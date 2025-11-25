03701ddf6677afa993e5d4c8644f92aa2c3c6372
ambrosestarlit <ambrose.starlit@gmail.com>
2025-11-25 12:09:56 +0900

    1

---
+++ service-worker.js
@@ -0,0 +1,87 @@
+// Snow Overlay Generator - Service Worker
+const CACHE_NAME = 'snow-overlay-generator-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js',
+  './js/effect_snow.js',
+  './js/utils.js',
+  './fonts/JK-Maru-Gothic-M.otf',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.log('Cache addAll failed:', err);
+      })
+  );
+  self.skipWaiting();
+});
+
+// アクティベート時に古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// フェッチ時にキャッシュから返す（ネットワークファースト戦略）
+self.addEventListener('fetch', (event) => {
+  // CDNからのリソースはネットワークファースト
+  if (event.request.url.includes('cdnjs.cloudflare.com')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  // ローカルリソースはキャッシュファースト
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
